26ef77442903492221c7c23504a33cbc860feef7
Stewart Morgan <stewartmorgan86@gmail.com>
2022-06-02 23:27:29 +0100

    start on demographics

--- cypress/integration/api-endpoints/Demographics/tests.js
+++ cypress/integration/api-endpoints/Demographics/tests.js
@@ -1,1 +1,91 @@
-// TODO: Add scripted tests
+const ApiBaseClass = require("../../../classes/api-base-class");
+let controller;
+let swaggerResponse;
+let endpoints;
+const JWTs = {
+    userJWT: "",
+    adminJWT: "",
+};
+
+before(() => {
+    // Act
+    cy.getSwaggerData().then((swaggerData) => {
+        swaggerResponse = swaggerData;
+        controller = new ApiBaseClass(swaggerData);
+    });
+
+    cy.fixture("users").then((userDetails) => {
+        const adminUserData = {
+            username: userDetails.admin_username,
+            password: userDetails.admin_password,
+            organisation: "Collaborative Partners",
+            authentication: "Demo",
+        };
+        cy.getJWT(adminUserData).then((jwtToken) => {
+            JWTs.adminJWT = "JWT " + jwtToken;
+        });
+        const userData = {
+            username: userDetails.username,
+            password: userDetails.password,
+            organisation: "Collaborative Partners",
+            authentication: "Demo",
+        };
+        cy.getJWT(userData).then((jwtToken) => {
+            JWTs.userJWT = "JWT " + jwtToken;
+        });
+    });
+});
+
+describe("Test Setup", () => {
+    it("Is Swagger Data available?", () => {
+        expect(swaggerResponse.length).to.be.at.least(1);
+    });
+
+    it("User Token is available to use", () => {
+        cy.expect(JWTs.userJWT.length).to.be.at.least(1);
+    });
+
+    it("Admin Token is available to use", () => {
+        cy.expect(JWTs.adminJWT.length).to.be.at.least(1);
+    });
+});
+
+describe("Test Endpoint", () => {
+    it("Check if endpoints to test", () => {
+        endpoints = controller.orderedEndpointData.filter((x) => {
+            return x.tags.includes("Demographics");
+        });
+        cy.expect(endpoints.length).to.be.at.least(1);
+    });
+
+    it("Check if endpoints are valid", () => {
+        endpoints.forEach((endpoint) => {
+            cy.expect(endpoint.endpoint).to.be.a("string");
+            cy.expect(endpoint.requestType).to.be.a("string");
+        });
+    });
+
+    it("Check endpoint for 401 status", () => {
+        endpoints.forEach((endpoint) => {
+            cy.apiRequest(endpoint, null).then((response) => {
+                cy.expect(response.status).to.be.equal(401);
+            });
+        });
+    });
+
+    // TODO: Test endpoint for 400 status
+
+    it("Check endpoint for 403 status", () => {
+        endpoints.forEach((endpoint) => {
+            if (endpoint.responses["403"]) {
+                cy.apiRequest(endpoint, JWTs.userJWT).then((response) => {
+                    cy.expect(response.status).to.be.equal(403);
+                });
+            }
+        });
+    });
+
+    // TODO: Test endpoint for 404 status
+
+    // TODO: Test endpoint for 200 status
+});
