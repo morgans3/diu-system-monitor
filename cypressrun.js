b677b2ee5b6a20e6e47bfb1c1d8f2af972c8d350
alexseymourdev <alexseymourdev@gmail.com>
2022-06-07 10:33:47 +0100

    updating fixture creation

--- cypressrun.js
+++ cypressrun.js
@@ -2,6 +2,7 @@ const cy2 = require("cy2");
 const fs = require("fs");
 const https = require("https");
 const { settings } = require("./settings");
+const createFixtures = require("./create-fixtures").createFixtureData;
 const testServerURL = process.env.TEST_SERVER_URL || "http://localhost:1234/";
 
 (async () => {
@@ -9,7 +10,7 @@ const testServerURL = process.env.TEST_SERVER_URL || "http://localhost:1234/";
     const AWSHelper = require("diu-data-functions").Helpers.Aws;
     try {
         const credentials = JSON.parse(await AWSHelper.getSecrets("cypressaccounts"));
-        fs.writeFileSync("./cypress/fixtures/users.json", JSON.stringify(credentials));
+        fs.writeFileSync("./cypress/fixtures/cypressaccounts.json", JSON.stringify(credentials));
 
         const jwtCredentials = JSON.parse(await AWSHelper.getSecrets("jwt"));
         fs.writeFileSync("./cypress/fixtures/jwtCredentials.json", JSON.stringify(jwtCredentials));
@@ -34,7 +35,7 @@ const testServerURL = process.env.TEST_SERVER_URL || "http://localhost:1234/";
             });
             res.on("end", () => {
                 fs.writeFileSync("./cypress/fixtures/swagggerjson.json", body);
-
+                createFixtures(body);
                 cy2.run(testServerURL, "cy2").then(() => {
                     console.log("Setup complete. Proceeding with Cypress...");
                 });
